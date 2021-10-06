e41395feff8276cc167d6dd840c2797a3c6a7649
Tickloop <pulkitarya2000@gmail.com>
2021-10-06 12:15:35 -0400

    Added loggers for activity lifecycle states.

--- app/src/main/java/com/tickloop/t33d/MainActivity.java
+++ app/src/main/java/com/tickloop/t33d/MainActivity.java
@@ -3,12 +3,50 @@ package com.tickloop.t33d;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivityTAG";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: On Create was fired by the Android Framework");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: On Start was fired by the Android Framework");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: On Resume was fired by the Android Framework");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: On Pause was fired by the Android Framework");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: On Stop was fired by the Android Framework");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: On Destroy was fired by the Android Framework");
+    }
+
+    public void sendMessage(View view){
+        // This is used to facilitate logging in
+        Log.d(TAG, "sendMessage: Send Message was fired. User clicked the button");
     }
 }
