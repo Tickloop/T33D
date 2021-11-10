9b2e1e95a9d15a01e915f9ec4b21bc55ff2b750f
Tickloop <pulkitarya2000@gmail.com>
2021-11-10 18:41:19 -0500

    Added click listener in Lobby Activity.

--- app/src/main/java/com/tickloop/t33d/LobbyActivity.java
+++ app/src/main/java/com/tickloop/t33d/LobbyActivity.java
@@ -2,13 +2,26 @@ package com.tickloop.t33d;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class LobbyActivity extends AppCompatActivity {
-
+    private static final String TAG = "LobbyActivityTAG";
+    private final View.OnClickListener launchGame = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.d(TAG, "onClick: Launch Game was fired");
+            Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+            startActivity(intent);
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+        ((Button) findViewById(R.id.play_button)).setOnClickListener(launchGame);
     }
 }
