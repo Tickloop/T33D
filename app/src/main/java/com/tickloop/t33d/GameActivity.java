e5db172b4e0df79083b5a18450bea8cca9e42212
samreid15 <reid.499@osu.edu>
2021-11-15 13:31:03 -0500

    Added features to lobby and game to add users and determine the game winner.

--- app/src/main/java/com/tickloop/t33d/GameActivity.java
+++ app/src/main/java/com/tickloop/t33d/GameActivity.java
@@ -2,13 +2,39 @@ package com.tickloop.t33d;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
-
+    private static final String TAG = "GameActivityTAG";
+    private String winner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        Bundle extras = getIntent().getExtras();
+        ArrayList<String> playerList = extras.getStringArrayList("playerList");
+        if(playerList.size() !=0){
+            int randomVal = (int)(Math.random()*playerList.size());
+            winner =playerList.get(randomVal);
+        }
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        TextView text = findViewById(R.id.user_pl_text);
+        if(text!=null) {
+            text.setText(winner);
+            boolean show = text.isShown();
+            Log.d("onStart", "Is shown = " + show);
+            Log.d("onStart", "Text set to show winner");
+        }
+        Log.d(TAG, "onStart: On Start was fired by the Android Framework");
     }
 }
--- app/src/main/java/com/tickloop/t33d/LobbyActivity.java
+++ app/src/main/java/com/tickloop/t33d/LobbyActivity.java
@@ -1,27 +1,138 @@
 package com.tickloop.t33d;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.Fragment;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.hardware.Sensor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class LobbyActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LobbyActivity extends Activity {
     private static final String TAG = "LobbyActivityTAG";
+    private ArrayList<String> playerNames = new ArrayList<String>();
     private final View.OnClickListener launchGame = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             Log.d(TAG, "onClick: Launch Game was fired");
+
             Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+            intent.putExtra("playerList", playerNames);
             startActivity(intent);
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        playerNames.add("Sam");
+        playerNames.add("Jim");
+        playerNames.add("Tim");
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_lobby);
+        ListView players = findViewById(R.id.playerList);
+        ArrayAdapter<String> playerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, playerNames);
+        players.setAdapter(playerAdapter);
+        boolean lobbyChange = false;
+        String username="";
+        //get new username if needed
+        if(lobbyChange){
+            addPlayer(username, playerAdapter);
+        }
+
         ((Button) findViewById(R.id.play_button)).setOnClickListener(launchGame);
+        Button addPlayers = (Button) findViewById(R.id.add_player_button);
+        addPlayers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertBuild = new AlertDialog.Builder(LobbyActivity.this);
+                EditText playerInput = new EditText(LobbyActivity.this);
+                playerInput.setContentDescription("Enter Player Name");
+                alertBuild.setView(playerInput);
+                alertBuild.setPositiveButton("Add Player", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String inputPlayer = playerInput.getText().toString();
+                        addPlayer(inputPlayer, playerAdapter);
+                    }
+                });
+                alertBuild.show();
+            }
+        });
     }
+
+
+    public void addPlayer(String username, ArrayAdapter adapter){
+
+        adapter.add(username);
+    }
+
+    /*private class PlayerHolder extends RecyclerView.ViewHolder {
+        private final TextView mPlayerInfoTextView;
+
+        PlayerHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.activity_lobby, parent, false));
+
+            mPlayerInfoTextView = itemView.findViewById(R.id.playerTextView);
+        }
+
+        void bind() {
+            //String mDescriptionStr = getSensorDescription(sensor);
+            String playerName = playerNames.get(0);
+            mPlayerInfoTextView.setText(playerName);
+        }
+    }
+
+    private class PlayerAdapter extends RecyclerView.Adapter<PlayerHolder> {
+        private final List<String> mPlayerList;
+
+        PlayerAdapter(List<String> playerList) {
+            mPlayerList = playerList;
+        }
+        private final TextView playerTextView;
+        public ViewHolder(View view){
+            super(view);
+            playerTextView = (TextView) view.findViewById(R.id.playerTextView);
+
+        }
+
+
+
+        @NonNull
+        @Override
+        public PlayerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(requireActivity());
+            return new PlayerHolder(inflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull PlayerHolder holder, int position) {
+            String playerName = mPlayerList.get(position);
+            playerTextView.setText(playerName);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mPlayerList.size();
+        }
+    }*/
+
 }
